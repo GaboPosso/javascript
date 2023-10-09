6e6dfe9eb56bc54878730f7afbfbe8bb2686739d
Gabriel P <gabopos@gmail.com>
2023-10-08 22:44:31 -0500

    upload Menu Hamburguer

--- DOM Exercises/js/index_dom.js
+++ DOM Exercises/js/index_dom.js
@@ -1,7 +1,5 @@
-import hamburguerMenu from "./menu_hamburger.js";
-
+import hamburguerMenu from "./dom/menu_hamburger.js";
 const d = document;
-
-d.addEventListener("DOMContentLoaded", e=> {
-  hamburguerMenu(".panel-btn", ".panel");
-})
+d.addEventListener("DOMContentLoaded", (e) => {
+  hamburguerMenu(".panel-btn", ".panel", ".menu a");
+});
