ef7f3bb568c885e3e9f6dcea8cdd1015cfc78dbb
Gabriel P <gabopos@gmail.com>
2023-10-09 23:33:23 -0500

    Add Digital clock to section 1 in dom exercises

--- DOM Exercises/js/index_dom.js
+++ DOM Exercises/js/index_dom.js
@@ -1,5 +1,8 @@
 import hamburguerMenu from "./dom/menu_hamburger.js";
+import { digitalClock, alarm } from "./dom/reloj.js";
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburguerMenu(".panel-btn", ".panel", ".menu a");
+  digitalClock('#reloj', "#activar-reloj", '#desactivar-reloj')
 });
+
