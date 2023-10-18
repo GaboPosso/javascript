b51be632a1d34f25ba7b36686c0c39f1997663b2
Gabriel P <gabopos@gmail.com>
2023-10-17 23:31:30 -0500

    Upload alarm to Dom Exercises

--- DOM Exercises/js/index_dom.js
+++ DOM Exercises/js/index_dom.js
@@ -1,8 +1,10 @@
 import hamburguerMenu from "./dom/menu_hamburger.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
+
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburguerMenu(".panel-btn", ".panel", ".menu a");
-  digitalClock('#reloj', "#activar-reloj", '#desactivar-reloj')
+  digitalClock('#reloj', "#activar-reloj", '#desactivar-reloj');
+  alarm("assets/alarm.mp3", "#activar-alarma", "#desactivar-alarma");
 });
 
