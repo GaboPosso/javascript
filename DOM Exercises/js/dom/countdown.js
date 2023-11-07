3be395cfda2f244150e84f6f5eb18e27b13b9254
Gabriel P <gabopos@gmail.com>
2023-11-07 08:18:52 -0500

    Upload countdown

---
+++ DOM Exercises/js/dom/countdown.js
@@ -0,0 +1,37 @@
+const d = document;
+
+export function countdown(id, limitDate, message) {
+  const $countdown = d.getElementById(id),
+    countDownDate = new Date(limitDate).getTime();
+
+  let countdownTempo = setInterval(() => {
+    let now = new Date().getTime(),
+      limitTime = countDownDate - now,
+      days = Math.floor(limitTime / (1000 * 60 * 60 * 24)),
+      hours =
+       ( "0" +
+        Math.floor((limitTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2),
+      minutes = ( "0" +
+      Math.floor((limitTime % (1000 * 60 * 60)) / (1000 * 60))).slice(-2),
+      seconds = ( "0" +
+      Math.floor((limitTime % (1000 * 60)) / (1000))).slice(-2);
+
+    $countdown.innerHTML = `<h3>${days} days ${hours} hours ${minutes} minutes ${seconds} seconds</h3>`;
+    console.log(countDownDate, now, limitTime);
+
+    if(limitTime < 0) {
+      clearInterval(countdownTempo);
+      $countdown.innerHTML = `<h3>${message}</h3>`;
+    console.log(countDownDate, now, limitTime);
+    }
+  }, 1000);
+
+  // // let countDownDate = new Date("Jan 01, 2023 00:00:00").getTime();
+  // // let now = new Date().getTime();
+  // // let t = countDownDate - now;
+  // // let days = Math.floor(t/(1000*60*60*24));
+  // // let hours = Math.floor(t%(1000*60*60*24))
+  // // let timer = setInterval(() => {
+
+  // })
+}
--- DOM Exercises/js/index_dom.js
+++ DOM Exercises/js/index_dom.js
@@ -2,15 +2,17 @@ import hamburguerMenu from "./dom/menu_hamburger.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { shortcuts } from "./dom/teclado.js"; 
 import { moveBall } from "./dom/teclado.js";
-
+import { countdown } from "./dom/countdown.js";
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburguerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock('#reloj', "#activar-reloj", '#desactivar-reloj');
   alarm("assets/alarm.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "Nov 06, 2023 20:14", "Happy Birthday, you CodeHead 🤓!!");
 });
 
 d.addEventListener("keydown", e => {
   shortcuts(e);
   moveBall(e, ".ball", ".stage")
 })
+
