88c889e226c615908b2e36293fee479d564f758b
Gabriel P <gabopos@gmail.com>
2023-11-09 11:04:30 -0500

    upload Scroll button

--- DOM Exercises/js/index_dom.js
+++ DOM Exercises/js/index_dom.js
@@ -1,18 +1,23 @@
 import hamburguerMenu from "./dom/menu_hamburger.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
-import { shortcuts } from "./dom/teclado.js"; 
+import { shortcuts } from "./dom/teclado.js";
 import { moveBall } from "./dom/teclado.js";
 import { countdown } from "./dom/countdown.js";
+import scrollTopButton from "./dom/boton_scroll.js";
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburguerMenu(".panel-btn", ".panel", ".menu a");
-  digitalClock('#reloj', "#activar-reloj", '#desactivar-reloj');
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarm.mp3", "#activar-alarma", "#desactivar-alarma");
-  countdown("countdown", "Nov 06, 2023 20:14", "Happy Birthday, you CodeHead 🤓!!");
+  countdown(
+    "countdown",
+    "Nov 06, 2023 20:14",
+    "Happy Birthday, you CodeHead 🤓!!"
+  );
+  scrollTopButton('.scroll-top-btn')
 });
 
-d.addEventListener("keydown", e => {
+d.addEventListener("keydown", (e) => {
   shortcuts(e);
-  moveBall(e, ".ball", ".stage")
-})
-
+  moveBall(e, ".ball", ".stage");
+});
