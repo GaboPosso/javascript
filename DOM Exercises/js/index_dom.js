49a3e928b857fdd1f3d611750ad6c32983cd43a3
Gabriel P <gabopos@gmail.com>
2023-11-20 08:04:23 -0500

    Upload JavaScript Responsive Design

--- DOM Exercises/js/index_dom.js
+++ DOM Exercises/js/index_dom.js
@@ -5,6 +5,7 @@ import { moveBall } from "./dom/teclado.js";
 import { countdown } from "./dom/countdown.js";
 import scrollTopButton from "./dom/boton_scroll.js";
 import darkTheme from "./dom/dark_theme.js";
+import responsiveMedia from "./dom/objeto_responsive.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -13,10 +14,22 @@ d.addEventListener("DOMContentLoaded", (e) => {
   alarm("assets/alarm.mp3", "#activar-alarma", "#desactivar-alarma");
   countdown(
     "countdown",
-    "Nov 06, 2023 20:14",
+    "Sep 02, 2024 11:30",
     "Happy Birthday, you CodeHead 🤓!!"
   );
   scrollTopButton(".scroll-top-btn");
+  responsiveMedia(
+    "youtube",
+    "(min-width: 1024px)",
+    `<a href="https://youtu.be/6IwUl-4pAzc?si=bMOsFeKbzh6Qgh1B" target="_blank rel="noopener">Ver video</a>`,
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/6IwUl-4pAzc?si=KmXogtTsMPheOekx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+  );
+  responsiveMedia(
+    "gmaps",
+    "(min-width: 1024px)",
+    `<a href="https://maps.app.goo.gl/ndzFCECtaiqP8bKN6" target="_blank rel="noopener">Ver mapa</a>`,
+    `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.2238813907597!2d-75.51973622522065!3d5.067427038369696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e476ffa140f94a5%3A0x25810bc036523ce7!2sCatedral%20Bas%C3%ADlica%20Metropolitana%20de%20Nuestra%20Se%C3%B1ora%20del%20Rosario%20de%20Manizales!5e0!3m2!1ses!2sco!4v1700484798430!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+  );
 });
 
 d.addEventListener("keydown", (e) => {
