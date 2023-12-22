92439dd6ced5d1a41be556c38fb171ed7efcf306
Gabriel P <gabopos@gmail.com>
2023-12-21 23:16:29 -0500

    upload Scroll Spy

--- DOM Exercises/js/index_dom.js
+++ DOM Exercises/js/index_dom.js
@@ -14,6 +14,7 @@ import getGeolocation from "./dom/geolocalizacion.js";
 import searchFilter from "./dom/filtro_busqueda.js";
 import draw from "./dom/sorteo.js";
 import slider from "./dom/carrusel.js";
+import scrollSpy from "./dom/scroll_espia.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -45,6 +46,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   searchFilter(".card-filter",".card")
   draw("#winner-btn", ".player");
   slider();
+  scrollSpy();
 });
 
 d.addEventListener("keydown", (e) => {
