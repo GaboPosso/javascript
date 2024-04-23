03e03e54b54c0216e91529e317844a16090163c6
Gabriel P <gabopos@gmail.com>
2024-04-23 13:35:04 -0500

    Add Ajax: XMLHttpsRequest Exercise

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -3,9 +3,27 @@
     $xhr = document.getElementById("xhr"),
     $fragment = document.createDocumentFragment();
 
-  xhr.addEventListener("readystatechange", (e) => {});
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
 
-  xhr.open("GET"," https://jsonplaceholder.typicode.com/users"); 
+    if (xhr.status >= 200 && xhr.status < 300) {
+      console.log(xhr.responseText);
+      let json = JSON.parse(xhr.responseText);
 
-  xhr.send()
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $xhr.appendChild($fragment);
+    } else {
+      let message = xhr.statusText || "An error has ocurred.";
+      $xhr.innerHTML = `Error ${xhr.status}: ${message}`
+    }
+  });
+
+  xhr.open("GET", " https://jsonplaceholder.typicode.com/users");
+
+  xhr.send();
 })();
