bb667742e7d917c3197cc583414efbdfb9bda9a0
Gabriel P <gabopos@gmail.com>
2024-04-24 23:52:09 -0500

    Upload AJAX API FETCH Async exercise

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -33,7 +33,6 @@
     $fragment = document.createDocumentFragment();
 
   fetch("https://jsonplaceholder.typicode.com/users")
-    
     .then((res) => (res.ok ? res.json() : Promise.reject(res)))
     .then((json) => {
       // console.log(json);
@@ -52,4 +51,41 @@
     .finally(() => {
       console.log("NVM");
     });
+})();
+
+(() => {
+  const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+      json = await res.json();
+
+    // if (!res.ok) throw new Error("An Error has occurred in Data Request");
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
+    try {
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $fetch.appendChild($fragment);
+      let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+        json = await res.json();
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $fetchAsync.appendChild($fragment);
+    } catch (err) {
+      console.log(err);
+      let message = err.statusText || "An error has ocurred.";
+      $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
+    } finally {
+      console.log("NVM")
+    }
+  }
+  getData();
 })();
